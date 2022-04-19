3e6aebc4d6596ea81e27bb51a7fd0f834a427ed6
Trevor Adams <trevor.b.adams@gmail.com>
2022-04-18 21:03:21 -0700

    axial coord fix

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ class HexBoard:
     """Hexs essentially have three dimensions, w/o reference to real math let's say A B C are  NW/SE NE/SW N/S"""
 
 # four or five to a side
-    def __init__(self, size = 5, coords=None) -> None:
+    def __init__(self, size = 4, coords=None, ax_coords=None) -> None:
         if coords is None:
             coords = []
+        if ax_coords is None:
+            ax_coords = []
         first_row_length = size
         self.rows_no = (size * 2) - 1
         self.max_coord = self.rows_no // 2
@@ -19,10 +21,13 @@ class HexBoard:
                 range(first_row_length, middle_row_length + 1),
                 range(middle_row_length - 1, first_row_length - 1, -1))
         ]
+        self.center = (size-1, size-1)
         for coord in coords:
             row = coord[0]
             col = coord[1]
             self.rows[row][col] = True
+        for axcoord in ax_coords:
+            self.set_axial(*axcoord, True)
         self.cells = 0
         for row in self.rows:
             self.cells += len(row)
@@ -35,8 +40,12 @@ class HexBoard:
     #     q_index
 
     def axial_to_index(self, q, r, s):
+        # -3:0 -2: -1  -1 -2 0:-3, 1:-3, 2:-3
         row = r + self.max_coord
-        q_index = row + q
+        if row < self.max_coord:
+            q_index = row + q
+        else:
+            q_index = q + self.max_coord
 
         return row, q_index
 
@@ -48,6 +57,9 @@ class HexBoard:
 
     def get_index(self, row, col):
         return self.rows[row][col]
+
+    def set_index(self, row, col, val):
+        self.rows[row][col] = val
 
     def get_axial(self, q, r, s):
         coords = self.axial_to_index(q,r,s)
@@ -96,7 +108,6 @@ class HexBoard:
                     return False
         return True
 
-
     def length_of_slice_at_offset(self, offset):
         if offset > self.max_coord:
             raise IndexError("")
@@ -120,12 +131,18 @@ class HexBoard:
         queue = [self]
         opened = []
         log_interval = self.cells
+        score = (self.cells // 2) * (self.cells // 2)
         while len(queue) > 0:
             head = queue.pop()
             if head not in opened:
                 opened.append(head)
-                if len(opened) % log_interval == 0:
-                    print("opened {} nodes.".format(len(opened)))
+                score = min(score, head.score())
+                # print(head)
+                if head.is_finished():
+                    print("we did it")
+                    break
+                elif len(opened) % log_interval == 0:
+                    print("opened {} nodes, score {}.".format(len(opened), head.score()))
             for row_index in range(len(head.rows)):
                 for col in range(row_index):
                     next_node = head.touch_index(row_index, col)
@@ -133,6 +150,17 @@ class HexBoard:
                         queue.append(next_node)
 
 
+    def filled(self):
+        count = 0
+        for row in self.rows:
+            for value in row:
+                if value:
+                    count += 1
+        return count
+
+    def score(self):
+        count = self.filled()
+        return count * (self.cells - count)
 
 
     def __str__(self) -> str:
@@ -156,12 +184,14 @@ def print_touch(board, q, r, s):
     print("after touching {}, board is \n{}".format((q,r,s), board.touch(q,r,s)))
 
 if __name__ == '__main__':
-    board = HexBoard(coords=[(1,2),(4,5), (4,4)])
+    board = HexBoard(ax_coords=[(0,0,0), (1,0,-1), (0,-1,1), (1,-3,2)])
     print(board)
     print_value(board, 0,0,0)
     print_value(board, 1,0,-1)
     print_value(board, 1,-3,-1)
     print_value(board, 0, -1, 1)
+    print_value(board, -1,0,1)
     print ("neightbors of 0, 0, 0 are {}, AKA {}".format(board.neighbors_axial(0,0,0), board.index_neighbors_from_axial(0,0,0)))
     print_touch(board, 0,0,0)
     board.bfs()
+
